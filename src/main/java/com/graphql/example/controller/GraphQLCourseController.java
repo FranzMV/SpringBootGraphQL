1c558bdc9ae4fa17f9da5d7b33226a499168b214
franz <fmanzanedovalle@gmail.com>
2024-03-30 16:07:48 +0100

    add create and delete for courses

--- src/main/java/com/graphql/example/controller/GraphQLCourseController.java
+++ src/main/java/com/graphql/example/controller/GraphQLCourseController.java
@@ -1,9 +1,11 @@
 package com.graphql.example.controller;
 
 import com.graphql.example.entities.Course;
+import com.graphql.example.graphql.InputCourse;
 import com.graphql.example.service.ICourseService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.graphql.data.method.annotation.Argument;
+import org.springframework.graphql.data.method.annotation.MutationMapping;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
 import org.springframework.stereotype.Controller;
 
@@ -24,5 +26,20 @@ public class GraphQLCourseController {
     public List<Course> findAll(){
         return courseService.findAll();
 
+    }
+    @MutationMapping
+    public Course createCourse(@Argument InputCourse inputCourse){
+        Course course = new Course();
+        course.setName(inputCourse.getName());
+        course.setCategory(inputCourse.getCategory());
+        course.setTeacher(inputCourse.getTeacher());
+        courseService.createCourse(course);
+        return course;
+    }
+
+    @MutationMapping(name = "deleteCourseById")
+    public String deleteById(@Argument(name = "courseId") String id){
+        courseService.deleteById(Long.parseLong(id));
+        return "El curso con id "+ id+ " Ha sido eliminado";
     }
 }
