5f525f51ecef0cd5726faf8e1a8ef3af5ff20d44
franz <fmanzanedovalle@gmail.com>
2024-03-30 15:02:51 +0100

    add endPoint findAllStudents

--- src/main/java/com/graphql/example/controller/GraphQLStudentController.java
+++ src/main/java/com/graphql/example/controller/GraphQLStudentController.java
@@ -7,14 +7,22 @@ import org.springframework.graphql.data.method.annotation.Argument;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
 import org.springframework.stereotype.Controller;
 
+import java.util.List;
+
 @Controller
 public class GraphQLStudentController {
 
     @Autowired
     private IStudentService studentService;
+
     @QueryMapping(name = "findStudentById")
     public Student findByID(@Argument(name = "studentId") String id){
         Long studentId = Long.parseLong(id);
         return  studentService.findById(studentId);
+    }
+
+    @QueryMapping(name = "findAllStudents")
+    public List<Student> findAll(){
+        return studentService.findAll();
     }
 }
