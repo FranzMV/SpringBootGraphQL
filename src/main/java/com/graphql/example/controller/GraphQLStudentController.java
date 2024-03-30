a9ccfd32a49e0948139d8c5d888f5da12bb57f6e
franz <fmanzanedovalle@gmail.com>
2024-03-30 14:57:17 +0100

    add GraphQLStudentController with endpoint findStudentById

---
+++ src/main/java/com/graphql/example/controller/GraphQLStudentController.java
@@ -0,0 +1,20 @@
+package com.graphql.example.controller;
+
+import com.graphql.example.entities.Student;
+import com.graphql.example.service.IStudentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.graphql.data.method.annotation.Argument;
+import org.springframework.graphql.data.method.annotation.QueryMapping;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class GraphQLStudentController {
+
+    @Autowired
+    private IStudentService studentService;
+    @QueryMapping(name = "findStudentById")
+    public Student findByID(@Argument(name = "studentId") String id){
+        Long studentId = Long.parseLong(id);
+        return  studentService.findById(studentId);
+    }
+}
